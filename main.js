ebbddc5c54b7fe67df2b50002178f87a6c159577
Wanda <wandaplaza1031@gmail.com>
2022-12-09 23:32:09 -0500

    Final commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstName = "Wanda";
+const lastName = " Plaza";
+const age = 22;
+alert("My name is " + firstName + lastName);
+alert("My age is "  + age);
+console.log("I am having a fun time in class")
